afe64e1b5b0f50156401d8c448aec63df2e0da23
Martín Vigil <gagliotti@gmail.com>
2017-07-31 18:54:27 -0300

    Update TesteGit.java

--- TesteGit/src/testegit/TesteGit.java
+++ TesteGit/src/testegit/TesteGit.java
@@ -15,7 +15,7 @@ public class TesteGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("testegit.TesteGit.main()");
+        System.out.println("modificado!");
     }
     
 }
